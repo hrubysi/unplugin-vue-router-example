6bc37d801159d0fa11a523f38649bc1a864fdd6b
Šimon Hrubý <simon.hruby@designeo.cz>
2023-03-18 20:16:30 +0100

    Init

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {useRouter} from "vue-router/auto";
+
+const router = useRouter();
+router.push({name: ''});
+</script>
+
+<template>
+  <main>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink :to="{name: '/articles/[slug]', params: {slug: 'test'}}">Test article</RouterLink>
+  </main>
+</template>
